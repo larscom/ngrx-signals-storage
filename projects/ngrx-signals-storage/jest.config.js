a0d4f6716db21862b06ea7558bf1ceb61dc46d39
larscom <larskniep@gmail.com>
2025-08-14 11:04:13 +0200

    fix rehydrate from storage for bool values

--- projects/ngrx-signals-storage/jest.config.js
+++ projects/ngrx-signals-storage/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'jest-preset-angular',
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@stomp/rx-stomp)'],
-  setupFiles: ['./projects/ngrx-signals-storage/setup-jest.ts']
+  setupFiles: ['./projects/ngrx-signals-storage/setup-jest.ts'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/ngrx-signals-storage/package.json',
+    '<rootDir>/projects/ngrx-signals-storage/package.json'
+  ]
 }
